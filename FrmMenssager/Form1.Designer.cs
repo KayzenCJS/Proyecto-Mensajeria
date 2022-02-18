698eb49b60c5717449c0c9d470c0dde75afaecb3
KayzenG0 <carolayncjs@gmail.com>
2022-02-18 18:35:42 -0400

    gevi

---
+++ FrmMenssager/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace FrmMenssager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btbnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(68, 173);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(161, 20);
+            this.txtMensaje.TabIndex = 1;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(235, 171);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(66, 23);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(337, 173);
+            this.listBox1.TabIndex = 4;
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Location = new System.Drawing.Point(54, 35);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(43, 13);
+            this.labelUsuario.TabIndex = 5;
+            this.labelUsuario.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(57, 65);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 6;
+            // 
+            // btbnConectar
+            // 
+            this.btbnConectar.Location = new System.Drawing.Point(57, 115);
+            this.btbnConectar.Name = "btbnConectar";
+            this.btbnConectar.Size = new System.Drawing.Size(132, 23);
+            this.btbnConectar.TabIndex = 7;
+            this.btbnConectar.Text = "Conectar";
+            this.btbnConectar.UseVisualStyleBackColor = true;
+            this.btbnConectar.Click += new System.EventHandler(this.btbnConectar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 211);
+            this.Controls.Add(this.btbnConectar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.labelUsuario);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensaje);
+            this.Name = "Form1";
+            this.Text = "Mensajeria";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btbnConectar;
+    }
+}
+
---
+++ FrmMenssager/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using Transitions;
+
+
+
+
+namespace FrmMenssager
+{
+    public partial class Form1 : Form
+    {
+        static private NetworkStream stream;
+        static private StreamWriter streamw;
+        static private StreamReader streamr;
+        static private TcpClient cliente = new TcpClient();
+        static private string nick = "Desconocida";
+
+        private delegate void DaddItem(String s);
+        private void AddItem(string s)
+        {
+            listBox1.Items.Add(s);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Listen()
+        {
+            while (cliente.Connected)
+            {
+                try
+                {
+                    this.Invoke(new DaddItem(AddItem), streamr.ReadLine());
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("No se ha podido establecer una conexion con el servidor");
+                    Application.Exit();
+                }
+            }
+        }
+
+        void Conectar()
+        {
+            try
+            {
+                cliente.Connect("127.0.0.1",8000);
+                if (cliente.Connected)
+                {
+                    Thread t = new Thread(Listen);
+
+                    stream=cliente.GetStream();
+                    streamw = new StreamWriter(stream);
+                    streamr = new StreamReader(stream);
+
+                    streamw.WriteLine(nick);
+                    streamw.Flush();
+
+                    t.Start();
+
+                }
+                else
+                {
+                    MessageBox.Show("Servidor no Disponible! ");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Servidor no disponible");
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 329; 250 -position
+            btnEnviar.Location = new Point(-329, 250);
+            listBox1.Location = new Point(-329, 23);
+            txtMensaje.Location = new Point(-329, 250);
+
+
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            streamw.WriteLine(txtMensaje.Text);
+            streamw.Flush();
+            txtMensaje.Clear();
+        }
+
+        private void btbnConectar_Click(object sender, EventArgs e)
+        {
+            nick = txtUsuario.Text;
+            Conectar();
+
+            Transition t = new Transition(new TransitionType_EaseInEaseOut(900));
+            t.add(labelUsuario, "Left",555);
+            t.add(txtUsuario, "Left", 555);
+
+            t.add(btnEnviar, "Left", 283);
+            t.add(btbnConectar, "Left", 555);
+            t.add(listBox1, "Left", 26);
+            t.add(txtMensaje, "Left", 26);
+            t.run();
+        }
+    }
+}
